760a94f52e1ecdd298031464f8859174045281c3
proudparrot2 <hi@proudparrot2.tech>
2024-04-01 22:19:50 -0500

    move onClick function to buttons instead of icons

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -33,77 +33,77 @@ export default function Route() {
       <iframe class="w-screen h-[calc(100vh-4rem)]" ref={ref!} onLoad={handleLoad} />
 
       <div class="absolute bottom-2 z-40 join left-1/2 bg-base-200 -translate-x-1/2 rounded-m px-2">
-        <button class="btn btn-square join-item bg-base-200">
-          <ChevronLeft
-            class="h-5 w-5"
-            onClick={() => {
-              if (!ref || !ref.contentWindow) return
-              const contentWindow = ref.contentWindow as ContentWindow
+        <button
+          class="btn btn-square join-item bg-base-200"
+          onClick={() => {
+            if (!ref || !ref.contentWindow) return
+            const contentWindow = ref.contentWindow as ContentWindow
 
-              contentWindow.history.back()
-            }}
-          />
+            contentWindow.history.back()
+          }}
+        >
+          <ChevronLeft class="h-5 w-5" />
         </button>
-        <button class="btn btn-square join-item bg-base-200">
-          <RotateCw
-            class="h-5 w-5"
-            onClick={() => {
-              if (!ref || !ref.contentWindow) return
-              const contentWindow = ref.contentWindow as ContentWindow
+        <button
+          class="btn btn-square join-item bg-base-200"
+          onClick={() => {
+            if (!ref || !ref.contentWindow) return
+            const contentWindow = ref.contentWindow as ContentWindow
 
-              contentWindow.location.reload()
-            }}
-          />
+            contentWindow.location.reload()
+          }}
+        >
+          <RotateCw class="h-5 w-5" />
         </button>
         <A href="/">
           <button class="btn btn-square join-item bg-base-200">
             <Home class="h-5 w-5" />
           </button>
         </A>
-        <button class="btn btn-square join-item bg-base-200">
-          <ChevronRight
-            class="h-5 w-5"
-            onClick={() => {
-              if (!ref || !ref.contentWindow) return
-              const contentWindow = ref.contentWindow as ContentWindow
+        <button
+          class="btn btn-square join-item bg-base-200"
+          onClick={() => {
+            if (!ref || !ref.contentWindow) return
+            const contentWindow = ref.contentWindow as ContentWindow
 
-              contentWindow.history.forward()
-            }}
-          />
+            contentWindow.history.forward()
+          }}
+        >
+          <ChevronRight class="h-5 w-5" />
         </button>
         <input value={url()} type="text" class="input join-item w-80 bg-base-200 focus:outline-none " />
-        <button class="btn btn-square join-item bg-base-200">
-          <FileCode
-            class="h-5 w-5"
-            onClick={() => {
-              if (!ref || !ref.contentWindow) return
-              const contentWindow = ref.contentWindow as ContentWindow
+        <button
+          class="btn btn-square join-item bg-base-200"
+          onClick={() => {
+            if (!ref || !ref.contentWindow) return
+            const contentWindow = ref.contentWindow as ContentWindow
 
-              if (contentWindow.eruda?._isInit) {
-                contentWindow.eruda.destroy()
-              } else {
-                var erudaScript = contentWindow.document.createElement('script')
-                erudaScript.src = 'https://cdn.jsdelivr.net/npm/eruda'
-                erudaScript.onload = () => {
-                  if (!contentWindow) return
-                  contentWindow.eruda.init()
-                  contentWindow.eruda.show()
-                }
-                contentWindow.document.body.appendChild(erudaScript)
+            if (contentWindow.eruda?._isInit) {
+              contentWindow.eruda.destroy()
+            } else {
+              var erudaScript = contentWindow.document.createElement('script')
+              erudaScript.src = 'https://cdn.jsdelivr.net/npm/eruda'
+              erudaScript.onload = () => {
+                if (!contentWindow) return
+                contentWindow.eruda.init()
+                contentWindow.eruda.show()
               }
-            }}
-          />
+              contentWindow.document.body.appendChild(erudaScript)
+            }
+          }}
+        >
+          <FileCode class="h-5 w-5" />
         </button>
-        <button class="btn btn-square join-item bg-base-200">
-          <SquareArrowOutUpRight
-            class="h-5 w-5"
-            onClick={() => {
-              if (!ref || !ref.contentWindow) return
-              const contentWindow = ref.contentWindow as ContentWindow
+        <button
+          class="btn btn-square join-item bg-base-200"
+          onClick={() => {
+            if (!ref || !ref.contentWindow) return
+            const contentWindow = ref.contentWindow as ContentWindow
 
-              openAbWindow(contentWindow.location.href)
-            }}
-          />
+            openAbWindow(contentWindow.location.href)
+          }}
+        >
+          <SquareArrowOutUpRight class="h-5 w-5" />
         </button>
       </div>
     </div>
