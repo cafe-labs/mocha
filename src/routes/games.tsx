e638a012fdb617215afe3f254f0078387a4db990
proudparrot2 <hi@proudparrot2.tech>
2024-04-02 20:32:35 -0500

    add search to games

--- src/routes/games.tsx
+++ src/routes/games.tsx
@@ -4,6 +4,7 @@ import { GameData } from '../lib/types'
 
 export default function Games() {
   const [data, setData] = createSignal<GameData[]>([])
+  const [search, setSearch] = createSignal('')
 
   onMount(() => {
     fetch('/games.json')
@@ -14,10 +15,13 @@ export default function Games() {
   })
 
   return (
-    <div class="flex flex-wrap justify-center px-4 py-8 gap-4">
-      {data().map((game) => {
-        return <Game game={game} />
-      })}
+    <div class="flex flex-col items-center gap-2 py-4">
+      <input type="text" class="input input-bordered w-1/3" value={search()} onInput={(e) => setSearch(e.target.value)} placeholder={`Search ${data().length} games`} />
+      <div class="flex flex-wrap justify-center gap-4 px-4 py-8">
+        {data().map((game) => {
+          if (game.name.includes(search())) return <Game game={game} />
+        })}
+      </div>
     </div>
   )
 }
