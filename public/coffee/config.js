415677fe571a2da53e5934e6e7be97679a7f430b
proudparrot2 <hi@proudparrot2.tech>
2024-04-03 17:36:48 -0500

    update bare url

--- public/coffee/config.js
+++ public/coffee/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/~/',
-    bare: 'https://tomp.app',
+    bare: '/-/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
