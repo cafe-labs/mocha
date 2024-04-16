3d0923ec4c4e2864590418c269af9f71e5243ad7
proudparrot2 <hi@proudparrot2.tech>
2024-04-15 21:52:53 -0500

    Add fuzzy search to games

--- src/routes/games.tsx
+++ src/routes/games.tsx
@@ -1,10 +1,19 @@
+import Fuse, { FuseResult } from 'fuse.js'
 import { createSignal, onMount } from 'solid-js'
 import Game from '../components/game'
 import { GameData } from '../lib/types'
 
 export default function Games() {
   const [data, setData] = createSignal<GameData[]>([])
-  const [search, setSearch] = createSignal('')
+  const [results, setResults] = createSignal<FuseResult<GameData>[]>([])
+
+  function handleSearch(text: string) {
+    const search = data()
+    const fuse = new Fuse(search, {
+      keys: ['name']
+    })
+    setResults(fuse.search(text))
+  }
 
   onMount(() => {
     fetch('/games.json')
@@ -16,10 +25,12 @@ export default function Games() {
 
   return (
     <div class="flex flex-col items-center gap-2 py-4">
-      <input type="text" class="input input-bordered w-1/3" value={search()} onInput={(e) => setSearch(e.target.value)} placeholder={`Search ${data().length} games`} />
+      <input type="text" class="input input-bordered w-1/3" onInput={(e) => handleSearch(e.target.value)} placeholder={`Search ${data().length} games`} />
       <div class="flex flex-wrap justify-center gap-4 px-4 py-8">
-        {data().map((game) => {
-          if (game.name.includes(search())) return <Game game={game} />
+        {results().length > 0 ? results().map((result) => {
+          return <Game game={result.item} />
+        }) : data().map((game) => {
+          return <Game game={game} />
         })}
       </div>
     </div>
