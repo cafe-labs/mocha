d5a2fe04e6ff1b454181fe356c49acc855903c77
proudparrot2 <hi@proudparrot2.tech>
2024-06-25 00:45:31 -0500

    Control bar tooltips

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -90,44 +90,47 @@ export default function Route() {
   }
   return (
     <div>
-      <iframe class="h-screen w-screen" ref={ref!} onLoad={handleLoad} />
+      <iframe class="h-screen w-screen fixed" ref={ref!} onLoad={handleLoad} />
 
       <div class={clsx('rounded-m join fixed left-1/2 z-40 -translate-x-1/2 bg-base-200 px-2 transition-[bottom] duration-300', showControls() ? 'bottom-2' : '-bottom-16')}>
-        <button
-          class="btn btn-square join-item bg-base-200"
-          onClick={() => {
-            if (!ref || !ref.contentWindow) return
-            const contentWindow = ref.contentWindow as ContentWindow
+        <div class="tooltip" data-tip="Go back">
+          <button
+            class="btn btn-square join-item bg-base-200"
+            onClick={() => {
+              if (!ref || !ref.contentWindow) return
+              const contentWindow = ref.contentWindow as ContentWindow
+              contentWindow.history.back()
+            }}
+          >
+            <ChevronLeft class="h-5 w-5" />
+          </button>
+        </div>
 
-            contentWindow.history.back()
-          }}
-        >
-          <ChevronLeft class="h-5 w-5" />
-        </button>
+        <div class="tooltip" data-tip="Reload">
+          <button
+            class="btn btn-square join-item bg-base-200"
+            onClick={() => {
+              if (!ref || !ref.contentWindow) return
+              const contentWindow = ref.contentWindow as ContentWindow
+              contentWindow.location.reload()
+            }}
+          >
+            <RotateCw class="h-5 w-5" />
+          </button>
+        </div>
+        <div class="tooltip" data-tip="Go forward">
+          <button
+            class="btn btn-square join-item bg-base-200"
+            onClick={() => {
+              if (!ref || !ref.contentWindow) return
+              const contentWindow = ref.contentWindow as ContentWindow
 
-        <button
-          class="btn btn-square join-item bg-base-200"
-          onClick={() => {
-            if (!ref || !ref.contentWindow) return
-            const contentWindow = ref.contentWindow as ContentWindow
-
-            contentWindow.location.reload()
-          }}
-        >
-          <RotateCw class="h-5 w-5" />
-        </button>
-
-        <button
-          class="btn btn-square join-item bg-base-200"
-          onClick={() => {
-            if (!ref || !ref.contentWindow) return
-            const contentWindow = ref.contentWindow as ContentWindow
-
-            contentWindow.history.forward()
-          }}
-        >
-          <ChevronRight class="h-5 w-5" />
-        </button>
+              contentWindow.history.forward()
+            }}
+          >
+            <ChevronRight class="h-5 w-5" />
+          </button>
+        </div>
 
         <input
           value={url()}
@@ -141,60 +144,67 @@ export default function Route() {
             e.currentTarget.blur()
           }}
         />
+        <div class="tooltip" data-tip="Return to home screen">
+          <A class="btn btn-square join-item bg-base-200" href="/">
+            <Home class="h-5 w-5" />
+          </A>
+        </div>
 
-        <A class="btn btn-square join-item bg-base-200" href="/">
-          <Home class="h-5 w-5" />
-        </A>
+        <div class="tooltip" data-tip="Toggle devtools">
+          <button
+            class="btn btn-square join-item bg-base-200"
+            onClick={() => {
+              if (!ref || !ref.contentWindow) return
+              const contentWindow = ref.contentWindow as ContentWindow
 
-        <button
-          class="btn btn-square join-item bg-base-200"
-          onClick={() => {
-            if (!ref || !ref.contentWindow) return
-            const contentWindow = ref.contentWindow as ContentWindow
-
-            if (contentWindow.eruda?._isInit) {
-              contentWindow.eruda.destroy()
-            } else {
-              var erudaScript = contentWindow.document.createElement('script')
-              erudaScript.src = 'https://cdn.jsdelivr.net/npm/eruda'
-              erudaScript.onload = () => {
-                if (!contentWindow) return
-                contentWindow.eruda.init()
-                contentWindow.eruda.show()
+              if (contentWindow.eruda?._isInit) {
+                contentWindow.eruda.destroy()
+              } else {
+                var erudaScript = contentWindow.document.createElement('script')
+                erudaScript.src = 'https://cdn.jsdelivr.net/npm/eruda'
+                erudaScript.onload = () => {
+                  if (!contentWindow) return
+                  contentWindow.eruda.init()
+                  contentWindow.eruda.show()
+                }
+                contentWindow.document.body.appendChild(erudaScript)
               }
-              contentWindow.document.body.appendChild(erudaScript)
-            }
-          }}
-        >
-          <FileCode class="h-5 w-5" />
-        </button>
+            }}
+          >
+            <FileCode class="h-5 w-5" />
+          </button>
+        </div>
+        <div class="tooltip" data-tip="Pop-out tab in about:blank">
+          <button
+            class="btn btn-square join-item bg-base-200"
+            onClick={() => {
+              if (!ref || !ref.contentWindow) return
+              const contentWindow = ref.contentWindow as ContentWindow
 
-        <button
-          class="btn btn-square join-item bg-base-200"
-          onClick={() => {
-            if (!ref || !ref.contentWindow) return
-            const contentWindow = ref.contentWindow as ContentWindow
-
-            openAbWindow(contentWindow.location.href, false)
-          }}
-        >
-          <SquareArrowOutUpRight class="h-5 w-5" />
-        </button>
-
-        <button
-          class="btn btn-square join-item bg-base-200"
-          onClick={() => {
-            setShowControls(false)
-          }}
-        >
-          <PanelBottomClose class="h-5 w-5" />
-        </button>
+              openAbWindow(contentWindow.location.href, false)
+            }}
+          >
+            <SquareArrowOutUpRight class="h-5 w-5" />
+          </button>
+        </div>
+        <div class="tooltip" data-tip="Minimize control bar">
+          <button
+            class="btn btn-square join-item bg-base-200"
+            onClick={() => {
+              setShowControls(false)
+            }}
+          >
+            <PanelBottomClose class="h-5 w-5" />
+          </button>
+        </div>
       </div>
 
-      <div class={clsx('fixed bottom-2 right-2 transition-opacity duration-300', showControls() ? 'opacity-0' : 'opacity-50')}>
-        <button class="btn btn-square btn-ghost" onClick={() => setShowControls(true)}>
-          <PanelBottomOpen />
-        </button>
+      <div class={clsx('fixed bottom-2 right-2 transition-opacity duration-300', showControls() ? 'opacity-0 pointer-events-none' : 'opacity-100')}>
+        <div class="tooltip tooltip-left" data-tip="Maximize control bar">
+          <button class="btn btn-square btn-ghost" onClick={() => setShowControls(true)}>
+            <PanelBottomOpen />
+          </button>
+        </div>
       </div>
     </div>
   )
